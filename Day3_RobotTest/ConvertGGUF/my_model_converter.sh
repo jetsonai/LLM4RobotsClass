8fbc35867a5142340e9c4f770473c865a659a95b
R SSAEM <96120477+jetsonai@users.noreply.github.com>
2024-12-18 12:31:57 +0900

    Update my_model_converter.sh

--- Day3_RobotTest/ConvertGGUF/my_model_converter.sh
+++ Day3_RobotTest/ConvertGGUF/my_model_converter.sh
@@ -1,3 +1,3 @@
-python3 /home/jetsonai/llama.cpp/convert_hf_to_gguf.py /home/jetsonai/models/rssaem/ --outtype f16 --outfile /home/jetsonai/models/rssaem/GGUF/llama-3.2-rssaem-bts-3B-f16.gguf
+python3 /home/jetsonai/llama.cpp/convert_hf_to_gguf.py /home/jetsonai/models/rssaem/ --outtype f16 --outfile /home/jetsonai/models/rssaem/llama-rssaem-bts-3.2-3B-f16.gguf
 
 
